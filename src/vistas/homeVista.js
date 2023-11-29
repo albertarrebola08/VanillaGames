b71e6596055bfaf6d6520c74f9955716fb474576
Albert Arrebola Sans <104431726+albertarrebola08@users.noreply.github.com>
2023-11-29 19:45:44 +0000

    modificando componenetes

--- src/vistas/homeVista.js
+++ src/vistas/homeVista.js
@@ -1,3 +1,56 @@
-export default {
-  template: '<h1>home</h1>'
-}
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#"
+        ><img
+          src=".images/logo.svg"
+          alt=""
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+
+        Vanilla Games</a
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">TOP5 Proyectos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">A cerca de</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
+          <li class="nav-item">
+            <button class="ms-2 btn btn-success">
+              Iniciar sesión
+              <i class="bi bi-box-arrow-in-right"></i>
+            </button>
+          </li>
+          <li class="nav-item">
+            <button class="ms-2 btn btn-outline-light">
+              Regístrate
+              <i class="bi bi-box-arrow-in-right"></i>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+
